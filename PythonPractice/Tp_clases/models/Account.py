a9cc45559e18e7a3bcac6f1aa115520c27df71c2
Ana <ana_laura-99@hotmail.com>
2019-10-22 00:32:18 -0300

    22/01/19

--- PythonPractice/Tp_clases/models/Account.py
+++ PythonPractice/Tp_clases/models/Account.py
@@ -10,4 +10,21 @@ class Account:
         self._client = client
 
     def to_string(self):
-        return "Number: " + str(self._number) + "\n Total: " + str(self._total) + "\n Date Time Client: " + str(self._date_time_client) + "\n Client:\n " + str(self._client.to_string())
+        return "Number: " + str(self._number) + "\n Total: " + str(self._total) + "\n Date Time Client: " + \
+               str(self._date_time_client) + "\n Client:\n " + str(self._client.to_string())
+
+#: retiro(importe), deposito(importe) y get_saldo.
+
+    def retirement(self, amount: int):
+        print("Retirando $" + str(amount) + " de la cuenta corriente...")
+        if amount < self._total:
+            self._total = self._total - amount
+        else:
+            return Exception("Fondos insuficientes para hacer el retiro")
+
+    def deposit(self, amount: int):
+        print("Depositando $" + str(amount) + " a la cuenta corriente...")
+        self._total = self._total + amount
+
+    def get_total(self):
+        return self._total
--- PythonPractice/Tp_clases/models/Client.py
+++ PythonPractice/Tp_clases/models/Client.py
@@ -1,18 +1,17 @@
 class Client:
     def __init__(self, name: str, age: int, email: str):
-        if self.validate_email():
+        if self.validate_email(name):
             self._name = name
             self._age = age
             self._email = email
         else:
             print("Email invalid. Doesn't have")
-
     
     def to_string(self):
         return "Name: " + str(self._name) + "\nAge: " + str(self._age) + "\nEmail: " + str(self._email)
  
-    def validate_email(self):
-        if self._name.find('@') < 0:
+    def __validate_email(self, name: str):
+        if name.find('@') < 0:
             return False
         else:
             return True
